7a4df70d56472756869b66cd1c3b4403adc3b53e
anutt12 <87488155+anutt12@users.noreply.github.com>
2021-11-28 17:01:18 -0700

    Update MostRecentBillIntentHandler.java

--- src/main/java/com/example/handlers/MostRecentBillIntentHandler.java
+++ src/main/java/com/example/handlers/MostRecentBillIntentHandler.java
@@ -47,7 +47,7 @@ public class MostRecentBillIntentHandler implements RequestHandler, InfoRetrieve
         URL url = new URL("https://api.propublica.org/congress/v1/bills/search.json?query=recent");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        conn.setRequestProperty("X-API-Key", "O1ZdWmc8x27g8x05YHkc0VYKHfCBYTTTuvDAt4Kn");
+        conn.setRequestProperty("X-API-Key", "<API-Key>");
         conn.setRequestProperty("Content-Type", "application/json");
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
